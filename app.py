f0dfb94137e9d82b5029e79e18cb37bf290d9b4c
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-06 13:57:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ print(data)
 st.title('Consulta de precio de Bitcoin en Binance')
 st.write('Precio actual:', price)
 
-# Consulta de precio por parte del usuario
-symbol_user = st.text_input('Ingrese un símbolo de moneda (por ejemplo, ETHUSDT):')
-if symbol_user:
-    params = {'symbol': symbol_user}
-    response = requests.get(url, params=params)
-    data = response.json()
-    if 'price' in data:
-        price_user = data['price']
-        st.write('Precio de', symbol_user, ':', price_user)
-    else:
-        st.write('Símbolo de moneda no válido:', symbol_user)
+# # Consulta de precio por parte del usuario
+# symbol_user = st.text_input('Ingrese un símbolo de moneda (por ejemplo, ETHUSDT):')
+# if symbol_user:
+#     params = {'symbol': symbol_user}
+#     response = requests.get(url, params=params)
+#     data = response.json()
+#     if 'price' in data:
+#         price_user = data['price']
+#         st.write('Precio de', symbol_user, ':', price_user)
+#     else:
+#         st.write('Símbolo de moneda no válido:', symbol_user)
